14cc7a9eff4b9c374d8e81bade888c2e1e0768b7
mustafa <mustafaalikhan757@gmail.com>
2025-02-09 15:58:25 +0530

    Some change

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
   const [hasCopied, setHasCopied] = useState(false);
 
   const handleCopy = () => {
-    navigator.clipboard.writeText('mustafaalikhan@gmail.com');
+    navigator.clipboard.writeText('mustafaalikhan757@gmail.com');
     setHasCopied(true);
 
     setTimeout(() => {
